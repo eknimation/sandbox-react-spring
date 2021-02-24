15b1f8daefe994c4454ea441e35150e975ca47c5
Ake <akekapon.san@dik.co.th>
2021-02-24 12:07:54 +0700

    Make a simple toggle.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
+import SimpleToggle from "./Components/Simpletoggle";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <h1>App</h1>
+      <SimpleToggle />
+    </div>
+  );
 }
 
 export default App;
